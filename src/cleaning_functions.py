f557514a308c7270170d9ea0e435c2f453260a0b
Lulua Lakdawala <luluayusuf@gmail.com>
2020-09-11 19:06:40 -0700

    data preprocessing

--- src/cleaning_functions.py
+++ src/cleaning_functions.py
@@ -2,6 +2,16 @@ import numpy as np
 import pandas as pd
 
 def brand_name(row):
+    """This function returns the brand name for the product or brand
+    being tweeted about
+    """
+    
+    apple_list = ['iPhone', 'iPad or iPhone App',
+                  'iPad','Apple',
+                  'Other Apple product or service']
+    google_list = ['Google', 'Android',
+                   'Android App', 'Other Google product or service']
+    
     if row in apple_list:
         return 'Apple'
     elif row in google_list:
@@ -10,6 +20,10 @@ def brand_name(row):
         return np.nan
     
 def emotion_name(row):
+    """This function returns the emotion being projected in the tweet
+    either: Positive, Negative or Neutral_or_unknown
+    """
+    
     if row == 'Positive emotion':
         return 'Positive'
     elif row == 'Negative emotion':
@@ -17,3 +31,14 @@ def emotion_name(row):
     else:
         return 'Neutral_or_unknown'
     
+def brand_name_for_missing_brannds(row):
+    """This function returns the brand name 
+    
+    """
+    from re import search
+    if search(r'Apple|iPad|iPhone', row['tweet_text']):
+        return 'Apple'
+    elif search(r'android|Google', row['tweet_text']):
+        return 'Google'
+    else:
+        return np.nan
