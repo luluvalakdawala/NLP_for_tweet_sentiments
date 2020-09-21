1b2ac930173a9807d111a9ca47839c8d2d743b45
Lulua Lakdawala <luluayusuf@gmail.com>
2020-09-20 23:56:21 -0700

    added preprocessing py file

---
+++ src/preprocessing_functions.py
@@ -0,0 +1,42 @@
+from nltk.corpus import stopwords, wordnet
+from nltk.tokenize import regexp_tokenize, word_tokenize, RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+
+def get_wordnet_pos(treebank_tag):
+    '''
+    Translate nltk POS to wordnet tags
+    '''
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+sw = stopwords.words('english')
+def doc_preparer(doc, stop_words=sw):
+    '''
+    
+    :param doc: a document from the satire corpus 
+    :return: a document string with words which have been 
+            lemmatized, 
+            parsed for stopwords, 
+            made lowercase,
+            and stripped of punctuation and numbers.
+    '''
+    
+    regex_token = RegexpTokenizer(r"([a-zA-Z]+(?:’[a-z]+)?)")
+    doc = regex_token.tokenize(doc)
+    doc = [word.lower() for word in doc]
+    doc = [word for word in doc if word not in stop_words]
+    doc = pos_tag(doc)
+    doc = [(word[0], get_wordnet_pos(word[1])) for word in doc]
+    lemmatizer = WordNetLemmatizer() 
+    doc = [lemmatizer.lemmatize(word[0], word[1]) for word in doc]
+    return ' '.join(doc)
+
